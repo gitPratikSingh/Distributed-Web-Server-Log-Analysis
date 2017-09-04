7a0c839eb3b0c20824b4b50080e966eac9f5c21d
Pratik <pratik.iem01@gmail.com>
2017-09-03 22:21:42 -0400

    Update FrequentHostAnalysis.py

--- Results/FrequentHostAnalysis.py
+++ Results/FrequentHostAnalysis.py
@@ -1,1 +1,17 @@
+"""
+This module explores the data to find the most frequent hosts 
+"""
 
+""""
+This module expects an RDD as input. It prints out the content statistics and saves the transformed RDD which
+contains all the tuples of hosts and its request count as the elements. This module is called from the main 
+after the completion of parsing step
+"""
+
+def frequentHost(accessLogs): 
+  frequentHosts=(accessLogs
+                .map(lambda x:(x.host,1))
+                .reduceByKey(lambda x,y:x+y)
+                .cache())
+  print 'The top 20 most frequent hosts are %s,total URI requests sent: %d' %()
+  frequentHosts.saveAsTextFile('hdfs://my-node:9000/Distributed-Web-Server-Analysis/FrequentHostAnalysis.txt')
