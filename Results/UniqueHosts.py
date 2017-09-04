93dcd8c508750b0c52b668aecab7d476b54cfb11
Pratik <pratik.iem01@gmail.com>
2017-09-03 22:44:20 -0400

    Update UniqueHosts.py

--- Results/UniqueHosts.py
+++ Results/UniqueHosts.py
@@ -1,1 +1,17 @@
+"""
+This module takes the parsed RDD as the only input and performs RDD transformations
+to get the count of distinct hosts recorded by the web server in the log.
+A file named 'UniqueHosts.txt' is saved for latter analysis
+"""
 
+def UniqueHosts(accessLogs):
+  """ Create a uniqueHosts RDD"""
+  uniqueHostsRdd=(accessLogs
+                  .map(lambda log:log.host)
+                  .cache())
+                      
+  for count in uniqueHostsRdd.distinct().count():
+    print 'Total Number of Unique Hosts : Count %d' %(count)
+  
+  """Save the RDD in a file"""
+  uniqueHostsRdd.distinct().saveAsTextFile('hdfs://my-node:9000/UniqueHosts.txt')
