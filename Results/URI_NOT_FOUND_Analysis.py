0698e17c6a922f38020996478ac0145ca4d603ec
Pratik <pratik.iem01@gmail.com>
2017-09-03 22:09:05 -0400

    Update URI_NOT_FOUND_Analysis.py

--- Results/URI_NOT_FOUND_Analysis.py
+++ Results/URI_NOT_FOUND_Analysis.py
@@ -1,1 +1,11 @@
+"""
+This module explores the 404 errors. This error happens due to invalid URLs. This module can be used to get all/top URI NOT FOUND errors.
+This module does the following analysis on top of the logs which are associated with these errors:
 
+1. It lists the 404 errors in a file and prints 20lines of such logs in the driver program
+2. It lists top 20 404 error end points/URIs
+3. It lists the hosts with most number of 404 errors
+4. It lists number of 404 error codes per day
+5. It lists hourly 404 response codes
+
+"""
