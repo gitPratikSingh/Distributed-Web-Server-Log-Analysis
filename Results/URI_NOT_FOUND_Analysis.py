f6890cf10e47923043f6c4b59c689946e1dbefa9
Pratik <pratik.iem01@gmail.com>
2017-09-03 23:23:32 -0400

    Update URI_NOT_FOUND_Analysis.py

--- Results/URI_NOT_FOUND_Analysis.py
+++ Results/URI_NOT_FOUND_Analysis.py
@@ -9,3 +9,55 @@ This module does the following analysis on top of the logs which are associated 
 5. It lists hourly 404 response codes
 
 """
+
+def ErrorResponseCodeAnalysis(accessLogs):
+  """ Create a errorResponseCode404 log RDD"""
+  errorResponseCode404Logs=(accessLogs
+                            .filter(lambda log:log.response_code==404)
+                            .cache())
+                      
+  for log in errorResponseCode404Logs.take(20):
+    print '404 Error: %s' %(log)
+  
+  """URIs with most number of 404 errors """
+  errorResponseCode404URI=(errorResponseCode404Logs
+                           .map(lambda log:(log.endpoint,1))
+                           .reduceByKey(lambda x,y:x+y)
+                           .cache())
+  
+  for URI, count in errorResponseCode404URI.takeOrdered(20, lambda x:-x[1]):
+    print 'URI : %s, Count : %d' %(URI, count)
+  
+  """Hosts with most number of 404 errors """
+  hostErrorResponseCode404Count=(errorResponseCode404Logs
+                           .map(lambda log:(log.host,1))
+                           .reduceByKey(lambda x,y:x+y)
+                           .cache())
+  
+  for host, count in hostErrorResponseCode404Count.takeOrdered(20, lambda x:-x[1]):
+    print 'URI : %s, Count : %d' %(host, count)
+ 
+  
+  """404 errors per day"""
+  errorResponseCode404CountperDay=(errorResponseCode404Logs
+                                   .map(lambda log:(log.datetime.date(),1))
+                                   .reduceByKey(lambda x,y:x+y)
+                                   .cache())
+  
+  for day, count in errorResponseCode404CountperDay.takeOrdered(20, lambda x:-x[1]):
+    print 'Day : %s, Count : %d' %(day, count)
+ 
+ 
+  
+  """ hourly 404 errors"""
+  errorResponseCode404CountperHour=(errorResponseCode404Logs
+                                   .map(lambda log:(log.datetime.hour(),1))
+                                   .reduceByKey(lambda x,y:x+y)
+                                   .cache())
+  
+  for hour, count in errorResponseCode404CountperHour.takeOrdered(20, lambda x:-x[1]):
+    print 'Hour : %s, Count : %d' %(hour, count)
+ 
+
+  """Save the RDD in a file"""
+  errorResponseCode404Logs.saveAsTextFile('hdfs://my-node:9000/URI_NOT_FOUND_Analysis.txt')
